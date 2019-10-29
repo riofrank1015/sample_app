1f050761d925feafb3b359ebc874c10a617e5f0c
Ryo <eintracht.frankbb18@gmail.com>
2019-10-29 19:26:58 +0900

    Use SSL and the Puma webserver in production

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,27 @@
-require 'test_helper'
+class UsersController < ApplicationController
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
-    assert_response :success
+  def show
+    @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,10 +10,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
-  end
+end
 
   test "valid signup information" do
   get signup_path
@@ -25,10 +22,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
   follow_redirect!
   assert_template 'users/show'
-  assert_not flash.FILL_IN
 end
-end
-  .
-  .
-  .
 end
