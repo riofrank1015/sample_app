967bdb365c1c6709d03198ac40c1c9c735cbf961
tsurutan <ishaninaritaina623@gmail.com>
2019-10-30 19:20:18 +0900

    fix config environments production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,9 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
-# Force all access to the app over SSL, use Strict-Transport-Security,
-# and use secure cookies.
-config.force_ssl = true
-
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
 end
