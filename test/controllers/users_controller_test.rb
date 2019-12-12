90dea03f7b7999ca6fe5459b62cc634802821a08
Ryo <eintracht.frankbb18@gmail.com>
2019-12-12 20:34:44 +0900

    Implement basic login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,27 +1,9 @@
-class UsersController < ApplicationController
+require 'test_helper'
 
-  def show
-    @user = User.find(params[:id])
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
 end
