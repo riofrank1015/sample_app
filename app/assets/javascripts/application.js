89c9bff3e5187229c1d06d6d45102ddc655c8195
Ryo <eintracht.frankbb18@gmail.com>
2019-12-20 20:43:04 +0900

    Initial Commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require rails-ujs
+//= require jquery
+//= require bootstrap
+//= require turbolinks
+//= require_tree .
---
+++ test/test_helper.rb
@@ -0,0 +1,27 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  fixtures :all
+
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+end
